572a667e72c12a7a48340152962ac6f9309d54aa
Angelo Pengue <apengue1@gmail.com>
2024-06-16 12:56:27 -0400

    Adds commonly used local settings to local.lua

--- lua/local.lua
+++ lua/local.lua
@@ -1,8 +1,10 @@
 local M = {}
 
 function M.apply_to_config(config)
-  -- Your custom machine specific configs go here  
+  -- Your custom machine specific configs go here
+
+  -- config.background[1].hsb.brightness = 0.2
+  -- config.font_size = 17.0
 end
 
 return M
-
